77358baaaae9e7c557296e6899f43d43052ce73a
Dmitry Kazhdan <dmitry@kazhdan.net>
2019-02-07 19:00:18 +0000

    Begin writing adjacency_list and per_type_edge computation

--- main.py
+++ main.py
@@ -3,24 +3,24 @@ from graph_pb2 import Graph
 from graph_pb2 import FeatureNode
 from dpu_utils.tfmodels import SparseGGNN
 from typing import List, Optional, Dict, Any
-
+from itertools import groupby
+import numpy as np
 
 
 # TODO: fixup typo in API
 # TODO: check what 'number' means in feature extractor
 
 
-
 # TODO: set correct parameter values
 def get_gnn_params():
 
     gnn_params = {}
-    gnn_params["n_edge_types"] = 3
-    gnn_params["hidden_size"] = 3
-    gnn_params["edge_features_size"] = {1: 5, 2: 4, 3: 3}  # Dict from edge type to feature size
+    gnn_params["n_edge_types"] = 10
+    gnn_params["hidden_size"] = 64
+    gnn_params["edge_features_size"] = {}  # Dict from edge type to feature size
     gnn_params["add_backwards_edges"] = True
     gnn_params["message_aggregation_type"] = "sum"
-    gnn_params["layer_timesteps"] = 8
+    gnn_params["layer_timesteps"] = [8]
     gnn_params["use_propagation_attention"] = True
     gnn_params["use_edge_bias"] = True
     gnn_params["graph_rnn_activation"] = "relu"
@@ -51,7 +51,13 @@ GUARDED_BY_NEGATION, LAST_LEXICAL_USE,
 
 # TODO: retrieve adjacency list, given graph
 def compute_adjacency_lists(graph) -> List[tf.Tensor]:
-    return None
+
+    sorted_edges = sorted(graph.edge, key=lambda x: (int(x.type), int(x.sourceId), int(x.destinationId)), reverse=False)
+    grouped = [list(g) for k, g in groupby(sorted_edges, lambda x: x.type)]
+    adjacency_tensors = [[[x.sourceId, x.destinationId] for x in group] for group in grouped]
+    adjacency_tensors = [tf.convert_to_tensor(np.asarray(data, np.int64), np.int64) for data in adjacency_tensors]
+
+    return adjacency_tensors
 
 
 # TODO: compute initial node embeddings
@@ -61,7 +67,22 @@ def compute_initial_node_embeddings(graph) -> tf.Tensor:
 
 # TODO: compute incoming/outgoing edges per type
 def compute_edges_per_type(graph, incoming=True) -> tf.Tensor:
-    return None
+
+    # n_nodes = ... #TODO: ensure ids are consecutive
+
+    # TODO: assumes node ids range from 0 to n_nodes. Ensure this assumption actually holds
+    n_nodes, n_edge_types = 10, 5
+    edges_matrix = np.zeros((n_nodes, n_edge_types), np.int64)
+
+    if incoming:
+        for e in graph.edge: edges_matrix[e.destinationId, e.type] += 1
+    else:
+        for e in graph.edge: edges_matrix[e.sourceId, e.type] += 1
+
+    edges_matrix = tf.convert_to_tensor(edges_matrix, np.int64)
+
+    return edges_matrix
+
 
 
 # TODO: compute edge features
@@ -81,6 +102,7 @@ def run_varnaming(graph, ggnn_model, var_name):
     return None
 
 
+
 def main(path):
 
   # Create sparse GGNN model
@@ -93,11 +115,9 @@ def main(path):
   # TODO: compute type embedding
 
   with open(path, "rb") as f:
+
     g = Graph()
     g.ParseFromString(f.read())
-
-    print(g)
-
 
     dropout_keep_rate = 1.0
     node_embeddings=compute_initial_node_embeddings(g)
@@ -113,7 +133,8 @@ def main(path):
 
 
 
-main("...")
+main("/Users/AdminDK/Dropbox/Part III Modules/R252 Machine Learning "
+     "for Programming/Practicals/features-javac-master/Test.java.proto")
 
 
 
