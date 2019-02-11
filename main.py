73e4aac4ae61052ee64ed94c7dac552b4e1a8f0b
Dmitry Kazhdan <dmitry@kazhdan.net>
2019-02-11 17:12:41 +0000

    Define model structure

--- main.py
+++ main.py
@@ -13,71 +13,123 @@ class model():
 
     def __init__(self):
 
-        self.params= get_gnn_params()
-        self.gnn_model = SparseGGNN(self.params)
+        self.params = get_gnn_params()
+        self.voc_size = 100
         self.graph = tf.Graph()
         self.sess = tf.Session(graph=self.graph)
-        self.placeholders = {}
-        self.weights = {}
-        self.ops = {}
-        self.make_model()
 
+        with self.graph.as_default():
+            self.gnn_model = SparseGGNN(self.params)
+            self.placeholders = {}
+            self.weights = {}
+            self.ops = {}
+            self.make_model()
 
+            init_op = tf.group(tf.global_variables_initializer(),
+                               tf.local_variables_initializer())
+            self.sess.run(init_op)
 
 
     def make_model(self):
 
         h_dim = self.params['hidden_size']
-        self.num_edge_types=10
 
+        # Actual variable name, as a padded sequence of tokens
         self.placeholders['var_name'] = tf.placeholder(tf.float32, [1, 1, 20], name='train_label')
 
+        # Padded graph node sub-token sequences
         self.placeholders['node_token_ids'] = tf.placeholder(tf.float32, [None, h_dim],
                                                                           name='node_features')
+
+        # Graph adjacency lists
         self.placeholders['adjacency_lists'] = [tf.placeholder(tf.int32, [None, 2], name='adjacency_e%s' % e)
-                                                for e in range(self.num_edge_types)]
-        self.placeholders['num_incoming_edges_per_type'] = tf.placeholder(tf.float32, [None, self.num_edge_types],
+                                                for e in range(self.params['n_edge_types'])]
+
+        # Graph of incoming edges per type
+        self.placeholders['num_incoming_edges_per_type'] = tf.placeholder(tf.float32, [None, self.params['n_edge_types']],
                                                                           name='num_incoming_edges_per_type')
 
-        self.placeholders['slot_ids'] = tf.placeholder(tf.int32, [None, 1], name='slot_tokens')
+        # Node identifiers of all graph nodes of the target variable
+        self.placeholders['slot_ids'] = tf.placeholder(tf.int32, [None], name='slot_tokens')
 
 
-        # self.placeholders['initial_node_representation'] = tf.keras.layers.Embedding(self.placeholders['node_token_ids'].shape, )
+
+        # Compute the embedding of input node sub-tokens
+        self.embedding_layer = tf.keras.layers.Embedding(self.voc_size, h_dim,  trainable=True)
+        self.placeholders['initial_node_representation'] = self.embedding_layer(self.placeholders['node_token_ids'])
 
 
+        # Average the sub-token embeddings for every node
+        self.placeholders['averaged_initial_representation'] = tf.reduce_mean(self.placeholders['initial_node_representation'],
+                                                                              axis=1)
+
+
+        # Run graph through GGNN
         self.placeholders['gnn_reps'] = self.gnn_model.sparse_gnn_layer(1.0,
-                                                                        self.placeholders['initial_node_representation'],
+                                                                        self.placeholders['averaged_initial_representation'],
                                                                         self.placeholders['adjacency_lists'],
                                                                         self.placeholders['num_incoming_edges_per_type'],
                                                                         self.placeholders['num_incoming_edges_per_type'],
                                                                         {})
 
 
-        self.placeholders['avg_reps'] = tf.expand_dims(tf.reduce_mean(tf.gather(self.placeholders['gnn_reps'],
-                                                                                self.placeholders['slot_ids']), axis=0), 0)
 
-        self.gru_layer = tf.keras.layers.GRU(20, activation='relu', return_sequences=True)
+        # Compute average of <SLOT> usage representations
+        self.placeholders['avg_reps'] = tf.expand_dims(tf.expand_dims(tf.reduce_mean(tf.gather(self.placeholders['gnn_reps'],
+                                                                                self.placeholders['slot_ids']), axis=0), 0), 0)
 
+
+        # Obtain output sequence by passing through a single GRU layer
+        self.gru_layer = tf.keras.layers.GRU(20, activation='relu', return_sequences=True, trainable=True)
         self.placeholders['output_seq'] = self.gru_layer(self.placeholders['avg_reps'])
 
 
 
+        # TODO: compute loss and optimizers properly (use maximum-likelihood objective)
         self.ops['loss'] = tf.reduce_mean(tf.squared_difference(self.placeholders['var_name'], self.placeholders['output_seq']))
-        my_opt = tf.train.GradientDescentOptimizer(learning_rate=0.02)
+        my_opt = tf.train.AdamOptimizer(learning_rate=0.02)
         self.train_step = my_opt.minimize(self.ops['loss'])
+
+
+
+    # TODO: fill in method
+    def make_train_step(self):
+        return None
 
 
 
     def train(self, graph_samples):
 
-        n_samples = 10
+        with self.graph.as_default():
 
-        for iteration in range(100):
-            for sample in range(n_samples):
+            n_samples = len(graph_samples)
 
-                graph = graph_samples[sample]
-                loss = self.sess.run([self.ops['loss'], self.train_step], feed_dict=graph)
-                print("Loss=", loss)
+            for iteration in range(20):
+                for sample in range(n_samples):
+
+                    graph = graph_samples[sample]
+                    loss = self.sess.run([self.ops['loss'], self.train_step], feed_dict=graph)
+                    print("Loss=", loss)
+
+
+
+    def sample_train(self):
+
+        graph_sample = {
+            self.placeholders['var_name']: np.ones((1, 1, 20), dtype=np.float32),
+            self.placeholders['node_token_ids']: np.zeros((3, 64)),
+            self.placeholders['num_incoming_edges_per_type']: np.zeros((3, self.params['n_edge_types']), dtype=np.float32),
+            self.placeholders['slot_ids']: np.zeros((4,), dtype=np.int32)
+        }
+
+        for i in range(self.params['n_edge_types']):
+            graph_sample[self.placeholders['adjacency_lists'][i]] = np.zeros((10, 2))
+
+
+        self.train([graph_sample])
+        print("Trained...")
+
+
 
 
 
@@ -88,7 +140,7 @@ def get_gnn_params():
     gnn_params["n_edge_types"] = 10
     gnn_params["hidden_size"] = 64
     gnn_params["edge_features_size"] = {}  # Dict from edge type to feature size
-    gnn_params["add_backwards_edges"] = True
+    gnn_params["add_backwards_edges"] = False
     gnn_params["message_aggregation_type"] = "sum"
     gnn_params["layer_timesteps"] = [8]
     gnn_params["use_propagation_attention"] = False
@@ -99,9 +151,6 @@ def get_gnn_params():
     gnn_params["use_edge_msg_avg_aggregation"] = False
 
     return gnn_params
-
-
-
 
 
 # TODO: retrieve adjacency list, given graph
@@ -136,11 +185,6 @@ def compute_edges_per_type(graph, incoming=True) -> tf.Tensor:
 
 
 
-
-
-
-
-
 def main(path):
 
   with open(path, "rb") as f:
@@ -148,15 +192,8 @@ def main(path):
     g = Graph()
     g.ParseFromString(f.read())
 
-    # dropout_keep_rate = 1.0
-    # node_embeddings=compute_initial_node_embeddings(g)
-    # adjacency_lists=compute_adjacency_lists(g)
-    # num_incoming_edges_per_type=compute_edges_per_type(g)
-    # num_outgoing_edges_per_type=compute_edges_per_type(g, False)
-    # edge_features=compute_edge_features(g)
-
-
     m = model()
+    m.sample_train()
 
 
 
