c8df3a58ecdf7a1e28cacd235fa50414a7eee0fa
Dmitry Kazhdan <dmitry@kazhdan.net>
2019-02-14 00:46:47 +0000

    Refactor + Add Sequence Inference

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ class model():
         self.voc_size = 100
         self.graph = tf.Graph()
         self.sess = tf.Session(graph=self.graph)
+        self.mode = 'train'
 
         with self.graph.as_default():
             self.embedding_size = 64
@@ -25,102 +26,140 @@ class model():
             self.ops = {}
             self.make_model()
             self.make_train_step()
+
             init_op = tf.group(tf.global_variables_initializer(),
                                tf.local_variables_initializer())
             self.sess.run(init_op)
 
 
+
+    # TODO: Add type embeddings
+    # TODO: Add batched iteration
+    # TODO: Consider other ways of handling variable-length sequences, besides padding
+
     def make_model(self):
 
-        h_dim = self.params['hidden_size']
-
-        # Padded graph node sub-token sequences
-        self.placeholders['node_token_ids'] = tf.placeholder(tf.int32, [None, h_dim],
-                                                                          name='node_features')
-
-        # Graph adjacency lists
-        self.placeholders['adjacency_lists'] = [tf.placeholder(tf.int32, [None, 2], name='adjacency_e%s' % e)
-                                                for e in range(self.params['n_edge_types'])]
-
-        # Graph of incoming edges per type
-        self.placeholders['num_incoming_edges_per_type'] = tf.placeholder(tf.float32, [None, self.params['n_edge_types']],
-                                                                          name='num_incoming_edges_per_type')
-
-        # Node identifiers of all graph nodes of the target variable
-        self.placeholders['slot_ids'] = tf.placeholder(tf.int32, [None], name='slot_tokens')
-
-        # Actual variable name, as a padded sequence of tokens
-        self.decoder_targets = tf.placeholder(tf.int32, [None, None], name='train_label')
-        self.decoder_targets_length = tf.placeholder(shape = (None,), dtype = tf.int32, name = 'decoder_targets_length')
+        self.make_inputs()
 
         # Compute the embedding of input node sub-tokens
         self.embedding_encoder = tf.Variable(tf.random_uniform([self.voc_size, self.embedding_size]))
         self.embedding_inputs = tf.nn.embedding_lookup(self.embedding_encoder, self.placeholders['node_token_ids'])
 
-
         # Average the sub-token embeddings for every node
-        self.placeholders['averaged_initial_representation'] = tf.reduce_mean(self.embedding_inputs,
-                                                                              axis=1)
+        self.placeholders['averaged_initial_representation'] = tf.reduce_mean(self.embedding_inputs, axis=1)
 
         # Run graph through GGNN
         self.gnn_model = SparseGGNN(self.params)
-        self.placeholders['gnn_reps'] = self.gnn_model.sparse_gnn_layer(1.0,
+        self.placeholders['gnn_representation'] = self.gnn_model.sparse_gnn_layer(1.0,
                                                                         self.placeholders['averaged_initial_representation'],
                                                                         self.placeholders['adjacency_lists'],
                                                                         self.placeholders['num_incoming_edges_per_type'],
                                                                         self.placeholders['num_incoming_edges_per_type'],
                                                                         {})
 
-
         # Compute average of <SLOT> usage representations
-        self.placeholders['avg_reps'] = tf.expand_dims(tf.reduce_mean(tf.gather(self.placeholders['gnn_reps'],
+        self.placeholders['avg_representation'] = tf.expand_dims(tf.reduce_mean(tf.gather(self.placeholders['gnn_representation'],
                                                                                 self.placeholders['slot_ids']), axis=0), 0)
 
-        self.target_mask = tf.placeholder(tf.float32, [None, None], name='train_label')
 
         # Obtain output sequence by passing through a single GRU layer
-        self.decoder_targets_length = tf.placeholder(shape=(None,), dtype=tf.int32,name='decoder_targets_length')
         self.embedding_decoder = tf.Variable(tf.random_uniform([self.voc_size, self.embedding_size]))
-        self.decoder_embedding_inputs = tf.nn.embedding_lookup(self.embedding_decoder, self.decoder_targets)
-
-        decoder_cell = tf.nn.rnn_cell.GRUCell(self.params['hidden_size'])
-        initial_state = self.placeholders['avg_reps']
-        projection_layer = tf.layers.Dense(self.voc_size, use_bias=False)
-
-        helper = tf.contrib.seq2seq.TrainingHelper(self.decoder_embedding_inputs, self.decoder_targets_length)
-        decoder = tf.contrib.seq2seq.BasicDecoder(decoder_cell, helper, initial_state=initial_state, output_layer=projection_layer)
-
-        self.decoder_outputs_train, _, _ = tf.contrib.seq2seq.dynamic_decode(decoder)
-        self.decoder_logits_train = self.decoder_outputs_train.rnn_output
-
-        crossent = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.decoder_targets, logits=self.decoder_logits_train)
-        loss = tf.reduce_sum(self.target_mask * crossent)
-
-        global_step = tf.Variable(0, name='global_step', trainable=False)
-
-        # Calculate and clip gradients
-        params = tf.trainable_variables()
-        gradients = tf.gradients(loss, params)
-        clipped_gradients, _ = tf.clip_by_global_norm(
-            gradients, 5.0)
-
-        # Optimization
-        optimizer = tf.train.AdamOptimizer(0.01)
-        train_op = optimizer.apply_gradients(
-            zip(clipped_gradients, params), global_step=global_step)
+        self.decoder_embedding_inputs = tf.nn.embedding_lookup(self.embedding_decoder, self.placeholders['decoder_targets'])
+        self.decoder_cell = tf.nn.rnn_cell.GRUCell(self.params['hidden_size'])
+        self.decoder_initial_state = self.placeholders['avg_representation']
 
 
-        print("Model built successfully...")
+        self.projection_layer = tf.layers.Dense(self.voc_size, use_bias=False)
 
 
-        # TODO: Add inference code to GRU layer for predictions
+        if self.mode == 'train':
+
+            # Define training sequence decoder
+            self.train_helper = tf.contrib.seq2seq.TrainingHelper(self.decoder_embedding_inputs,
+                                                                  self.placeholders['decoder_targets_length']
+                                                                  , time_major=True)
+
+            self.train_decoder = tf.contrib.seq2seq.BasicDecoder(self.decoder_cell, self.train_helper,
+                                                                 initial_state=self.decoder_initial_state,
+                                                                 output_layer=self.projection_layer)
+
+            self.decoder_outputs_train, _, _ = tf.contrib.seq2seq.dynamic_decode(self.train_decoder)
+            self.decoder_logits_train = self.decoder_outputs_train.rnn_output
+
+
+        else:
+
+            # Define inference sequence decoder
+            start_tokens = tf.tile(tf.constant([0], dtype=tf.int32), [self.batch_size])
+            end_token = 0
+
+            self.inference_helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(self.embedding_decoder,
+                                                              start_tokens=start_tokens, end_token=end_token)
+
+            max_iterations = tf.round(tf.reduce_max(self.encoder_inputs_length)) * 2
+
+            self.inference_decoder = tf.contrib.seq2seq.BasicDecoder(self.decoder_cell, self.inference_helper,
+                                                                     initial_state=self.decoder_initial_state,
+                                                                     output_layer=self.projection_layer)
+
+            self.outputs_inferece, _, _ = tf.contrib.seq2seq.dynamic_decode(self.inference_decoder,
+                                                                            maximum_iterations=max_iterations)
+
+            self.predictions = self.outputs_inference.sample_id
+
+
+
+        print ("Model built successfully...")
+
+
+
+    def make_inputs(self):
+
+        # Padded graph node sub-token sequences
+        self.placeholders['node_token_ids'] = tf.placeholder(tf.int32, [None, self.params['hidden_size']])
+
+        # Graph adjacency lists
+        self.placeholders['adjacency_lists'] = [tf.placeholder(tf.int32, [None, 2]) for _ in range(self.params['n_edge_types'])]
+
+        # Graph of incoming edges per type
+        self.placeholders['num_incoming_edges_per_type'] = tf.placeholder(tf.float32, [None, self.params['n_edge_types']])
+
+        # Node identifiers of all graph nodes of the target variable
+        self.placeholders['slot_ids'] = tf.placeholder(tf.int32, [None], name='slot_tokens')
+
+        # Actual variable name, as a padded sequence of tokens
+        self.placeholders['decoder_targets'] = tf.placeholder(tf.int32, [None, None], name='train_label')
+
+        # Specify output sequence lengths
+        self.placeholders['decoder_targets_length'] = tf.placeholder(shape=(None,), dtype=tf.int32)
+
+        # 0/1 matrix masking out tensor elements outside of the sequence length
+        self.placeholders['target_mask'] = tf.placeholder(tf.float32, [None, None])
+
 
 
 
 
     # TODO: fill in method
     def make_train_step(self):
-        return None
+
+
+        print(self.placeholders['decoder_targets'].shape)
+        print(self.decoder_logits_train.shape)
+
+        self.crossent = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.placeholders['decoder_targets'], logits=self.decoder_logits_train)
+        self.train_loss = tf.reduce_sum(self.placeholders['target_mask'] * self.crossent)
+        self.global_step = tf.Variable(0, name='global_step', trainable=False)
+
+        # Calculate and clip gradients
+        self.train_vars = tf.trainable_variables()
+        self.gradients = tf.gradients(self.train_loss, self.train_vars)
+        self.clipped_gradients, _ = tf.clip_by_global_norm(self.gradients, 5.0)
+
+        # Optimization
+        self.optimizer = tf.train.AdamOptimizer(0.01)
+        self.train_step = self.optimizer.apply_gradients(zip(self.clipped_gradients,self.train_vars))
+
 
 
 
@@ -130,13 +169,13 @@ class model():
 
             n_samples = len(graph_samples)
 
-            for iteration in range(20):
+            for iteration in range(100):
 
                 # Run one sample at a time
                 for sample in range(n_samples):
 
                     graph = graph_samples[sample]
-                    loss = self.sess.run([self.ops['loss'], self.train_step], feed_dict=graph)
+                    loss = self.sess.run([self.train_loss, self.train_step], feed_dict=graph)
                     print("Loss=", loss)
 
 
@@ -145,10 +184,12 @@ class model():
 
         # Sample graph
         graph_sample = {
-            self.placeholders['var_name']: np.ones((1, 1, 20), dtype=np.float32),
             self.placeholders['node_token_ids']: np.zeros((3, 64)),
             self.placeholders['num_incoming_edges_per_type']: np.zeros((3, self.params['n_edge_types']), dtype=np.float32),
-            self.placeholders['slot_ids']: np.zeros((4,), dtype=np.int32)
+            self.placeholders['slot_ids']: np.zeros((4,), dtype=np.int32),
+            self.placeholders['decoder_targets']: np.zeros((32, 1)),
+            self.placeholders['decoder_targets_length']: np.ones((1)),
+            self.placeholders['target_mask']: np.ones((32, 1))
         }
 
         for i in range(self.params['n_edge_types']):
