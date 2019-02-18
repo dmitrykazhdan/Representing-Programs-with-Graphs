4019c815bc6121909d9c25e0180c69bf17a4fe17
Dmitry Kazhdan <dmitry@kazhdan.net>
2019-02-18 01:30:12 +0000

    Develop graph pre-processing

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from dpu_utils.mlutils import Vocabulary
 from typing import List, Optional, Dict, Any
 from itertools import groupby
 import numpy as np
+from collections import defaultdict
 import os
 
 
@@ -16,7 +17,7 @@ class model():
 
     def __init__(self):
 
-        self.params = get_gnn_params()
+        self.params = self.get_gnn_params()
         self.voc_size = 100
         self.graph = tf.Graph()
         self.sess = tf.Session(graph=self.graph)
@@ -36,6 +37,33 @@ class model():
     # TODO: Add type embeddings
     # TODO: Add batched iteration
     # TODO: Consider other ways of handling variable-length sequences, besides padding
+
+
+    def make_inputs(self):
+
+        # Padded graph node sub-token sequences
+        self.placeholders['node_token_ids'] = tf.placeholder(tf.int32, [None, self.params['hidden_size']])
+
+        # Graph adjacency lists
+        self.placeholders['adjacency_lists'] = [tf.placeholder(tf.int32, [None, 2]) for _ in range(self.params['n_edge_types'])]
+
+        # Graph of incoming edges per type
+        self.placeholders['num_incoming_edges_per_type'] = tf.placeholder(tf.float32, [None, self.params['n_edge_types']])
+
+        # Node identifiers of all graph nodes of the target variable
+        self.placeholders['slot_ids'] = tf.placeholder(tf.int32, [None], name='slot_tokens')
+
+        #
+        self.placeholders['decoder_inputs'] = tf.placeholder(shape=(64, 1), dtype=tf.int32, name='dec_inputs')
+
+        # Actual variable name, as a padded sequence of tokens
+        self.placeholders['decoder_targets'] = tf.placeholder(dtype=tf.int32, shape=(1, 64), name='dec_targets')
+
+        # Specify output sequence lengths
+        self.placeholders['decoder_targets_length'] = tf.placeholder(shape=(1), dtype=tf.int32)
+
+        # 0/1 matrix masking out tensor elements outside of the sequence length
+        self.placeholders['target_mask'] = tf.placeholder(tf.float32, [None, None])
 
     def make_model(self):
 
@@ -64,7 +92,7 @@ class model():
 
         # Obtain output sequence by passing through a single GRU layer
         self.embedding_decoder = tf.get_variable('embedding_decoder', [self.voc_size, self.embedding_size])
-        self.decoder_embedding_inputs = tf.nn.embedding_lookup(self.embedding_decoder, self.placeholders['decoder_targets'])
+        self.decoder_embedding_inputs = tf.nn.embedding_lookup(self.embedding_decoder, self.placeholders['decoder_inputs'])
         self.decoder_cell = tf.nn.rnn_cell.GRUCell(self.params['hidden_size'])
         self.decoder_initial_state = self.placeholders['avg_representation']
 
@@ -83,8 +111,10 @@ class model():
                                                                  initial_state=self.decoder_initial_state,
                                                                  output_layer=self.projection_layer)
 
-            self.decoder_outputs_train, _, _ = tf.contrib.seq2seq.dynamic_decode(self.train_decoder)
+            self.decoder_outputs_train, e1, e2 = tf.contrib.seq2seq.dynamic_decode(self.train_decoder)
             self.decoder_logits_train = self.decoder_outputs_train.rnn_output
+
+            print(self.decoder_logits_train.shape)
 
 
         elif self.mode == 'infer':
@@ -114,34 +144,6 @@ class model():
 
         print ("Model built successfully...")
 
-
-
-    def make_inputs(self):
-
-        # Padded graph node sub-token sequences
-        self.placeholders['node_token_ids'] = tf.placeholder(tf.int32, [None, self.params['hidden_size']])
-
-        # Graph adjacency lists
-        self.placeholders['adjacency_lists'] = [tf.placeholder(tf.int32, [None, 2]) for _ in range(self.params['n_edge_types'])]
-
-        # Graph of incoming edges per type
-        self.placeholders['num_incoming_edges_per_type'] = tf.placeholder(tf.float32, [None, self.params['n_edge_types']])
-
-        # Node identifiers of all graph nodes of the target variable
-        self.placeholders['slot_ids'] = tf.placeholder(tf.int32, [None], name='slot_tokens')
-
-        # Actual variable name, as a padded sequence of tokens
-        self.placeholders['decoder_targets'] = tf.placeholder(tf.int32, [None, None], name='train_label')
-
-        # Specify output sequence lengths
-        self.placeholders['decoder_targets_length'] = tf.placeholder(shape=(None,), dtype=tf.int32)
-
-        # 0/1 matrix masking out tensor elements outside of the sequence length
-        self.placeholders['target_mask'] = tf.placeholder(tf.float32, [None, None])
-
-
-
-
     def make_train_step(self):
 
         self.crossent = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.placeholders['decoder_targets'], logits=self.decoder_logits_train)
@@ -159,17 +161,6 @@ class model():
 
 
 
-    def train(self, graph_samples):
-
-        with self.graph.as_default():
-
-            for iteration in range(100):
-
-                # Run one sample at a time
-                for graph in graph_samples:
-
-                    loss = self.sess.run([self.train_loss, self.train_step], feed_dict=graph)
-                    print("Loss:", loss)
 
 
 
@@ -177,111 +168,217 @@ class model():
 
         # Sample graph
         graph_sample = {
-            self.placeholders['node_token_ids']: np.zeros((3, 64)),
+            self.placeholders['node_token_ids']: np.random.randint(20, size=(3, 64)),
             self.placeholders['num_incoming_edges_per_type']: np.zeros((3, self.params['n_edge_types']), dtype=np.float32),
             self.placeholders['slot_ids']: np.zeros((4,), dtype=np.int32),
-            self.placeholders['decoder_targets']: np.zeros((32, 1)),
-            self.placeholders['decoder_targets_length']: np.ones((1)),
+            self.placeholders['decoder_targets']: np.random.randint(15, size=(1, 32)),
+            self.placeholders['decoder_inputs']: np.random.randint(15, size=(32, 1)),
+            self.placeholders['decoder_targets_length']: np.ones((1)) * 32,
             self.placeholders['target_mask']: np.ones((32, 1))
         }
 
         for i in range(self.params['n_edge_types']):
-            graph_sample[self.placeholders['adjacency_lists'][i]] = np.zeros((10, 2))
+            graph_sample[self.placeholders['adjacency_lists'][i]] = np.random.randint(3, size=(3, 2))
 
 
         self.train([graph_sample])
         print("Trained...")
 
+    def get_gnn_params(self):
+
+        gnn_params = {}
+        gnn_params["n_edge_types"] = 10
+        gnn_params["hidden_size"] = 64
+        gnn_params["edge_features_size"] = {}  # Dict from edge type to feature size
+        gnn_params["add_backwards_edges"] = True
+        gnn_params["message_aggregation_type"] = "sum"
+        gnn_params["layer_timesteps"] = [8]
+        gnn_params["use_propagation_attention"] = False
+        gnn_params["use_edge_bias"] = False
+        gnn_params["graph_rnn_activation"] = "relu"
+        gnn_params["graph_rnn_cell"] = "gru"
+        gnn_params["residual_connections"] = {}  #
+        gnn_params["use_edge_msg_avg_aggregation"] = False
+
+        return gnn_params
+
+
+    def compute_adjacency_lists(self, graph):
+
+        id_to_ind = {}
+
+        ind = 0
+        for node in graph.node:
+            id_to_ind[node.id] = ind
+            ind += 1
+
+        adj_lists = defaultdict(list)
+
+        for edge in graph.edge:
+            type_id = edge.type - 1
+            adj_lists[type_id].append([id_to_ind[edge.sourceId], id_to_ind[edge.destinationId]])
+
+        final_adj_lists = {edge_type: np.array(sorted(adj_list), dtype=np.int32)
+                           for edge_type, adj_list in adj_lists.items()}
+
+        return final_adj_lists
+
+
+
+    def compute_edges_per_type(self, graph, incoming=True):
+
+        # n_nodes = ... #TODO: ensure ids are consecutive
+
+        # TODO: assumes node ids range from 0 to n_nodes. Ensure this assumption actually holds
+        n_nodes, n_edge_types = 10, 5
+        edges_matrix = np.zeros((n_nodes, n_edge_types), np.int64)
+
+        if incoming:
+            for e in graph.edge: edges_matrix[e.destinationId, e.type] += 1
+        else:
+            for e in graph.edge: edges_matrix[e.sourceId, e.type] += 1
+
+        return edges_matrix
+
+
+
+    def compute_initial_node_representation(self, graph):
+
+        # Extract set of all sub-tokens and create their vocabulary
+        all_tokens = []
+
+        for n in graph.node:
+            all_tokens += split_identifier_into_parts(n.contents)
+
+        all_tokens = list(set(all_tokens))
+
+        self.vocabulary = Vocabulary.create_vocabulary(all_tokens, max_size=100, count_threshold=0, add_unk=True)
+        self.slot_id = len(self.vocabulary) + 1
+        max_size = 64
+        padding_element = 0
+
+
+
+        node_representations = np.array([self.vocabulary.get_id_or_unk_multiple(split_identifier_into_parts(node.contents),
+                                                                           max_size, padding_element)
+                                                                            for node in graph.node])
+
+
+
+        return node_representations
+
+
+
+    def get_var_nodes(self, graph):
+
+        var_nodes = {}
+
+        id_nodes = [node.id for node in graph.node if node.type == FeatureNode.IDENTIFIER_TOKEN]
+
+
+        for node in graph.node:
+            if node.type == FeatureNode.SYMBOL_VAR:
+                var_nodes[node.id] = []
+
+
+        for edge in graph.edge:
+            if edge.sourceId in var_nodes and edge.destinationId in id_nodes:
+                var_nodes[edge.sourceId].append(edge.destinationId)
+
+        return var_nodes
+
+
+
+    def create_sample(self, filepath):
+
+        with open(filepath, "rb") as f:
+
+            g = Graph()
+            g.ParseFromString(f.read())
+
+            adj_lists = self.compute_adjacency_lists(g)
+            node_reps = self.compute_initial_node_representation(g)
+            variable_nodes = self.get_var_nodes(g)
+
+            samples = []
+
+
+            for var_id in variable_nodes:
+                node_representation = node_reps.copy()
+
+                for var_node in variable_nodes[var_id]:
+                    node_representation[var_node, :] = 0
+                    node_representation[var_node, 0] = self.slot_id
+
+
+                target_mask = np.ones((self.params['hidden_size'], 1))
+                var_rep = node_representation[var_node]
+                for i in range(len(var_rep)):
+                    if var_rep[i] == 0: target_mask[i] = 0
+
+
+                graph_sample = {
+                    self.placeholders['node_token_ids']: node_representation,
+                    self.placeholders['num_incoming_edges_per_type']: np.zeros((node_representation.shape[0],
+                                                                                self.params['n_edge_types']),
+                                                                               dtype=np.float32),
+                    self.placeholders['slot_ids']: variable_nodes[var_id],
+                    self.placeholders['decoder_targets']: node_representation[var_node],
+                    self.placeholders['decoder_inputs']: node_representation[var_node],
+                    self.placeholders['decoder_targets_length']: np.ones((1)) * self.params['hidden_size'],
+                    self.placeholders['target_mask']: target_mask
+                }
+
+                print(len(adj_lists))
+                i = 0
+                for key in adj_lists:
+                    graph_sample[self.placeholders['adjacency_lists'][i]] = adj_lists[key]
+                    i += 1
+
+                samples.append(graph_sample)
+
+            return samples
 
 
 
 
 
-def get_gnn_params():
+    def get_graph_samples(self, dir_path):
 
-    gnn_params = {}
-    gnn_params["n_edge_types"] = 10
-    gnn_params["hidden_size"] = 64
-    gnn_params["edge_features_size"] = {}  # Dict from edge type to feature size
-    gnn_params["add_backwards_edges"] = True
-    gnn_params["message_aggregation_type"] = "sum"
-    gnn_params["layer_timesteps"] = [8]
-    gnn_params["use_propagation_attention"] = False
-    gnn_params["use_edge_bias"] = False
-    gnn_params["graph_rnn_activation"] = "relu"
-    gnn_params["graph_rnn_cell"] = "gru"
-    gnn_params["residual_connections"] = {}  #
-    gnn_params["use_edge_msg_avg_aggregation"] = False
+        graph_samples = []
 
-    return gnn_params
+        for dirpath, dirs, files in os.walk(dir_path):
+            for filename in files:
+                if filename[-5:] == 'proto':
+                    fname = os.path.join(dirpath, filename)
+                    graph_samples += self.create_sample(fname)
+
+        return graph_samples
 
 
 
-def compute_adjacency_lists(graph):
+    def train(self, path):
 
-    sorted_edges = sorted(graph.edge, key=lambda x: (int(x.type), int(x.sourceId), int(x.destinationId)), reverse=False)
-    grouped = [list(g) for k, g in groupby(sorted_edges, lambda x: x.type)]
-    adjacency_tensors = [[[x.sourceId, x.destinationId] for x in group] for group in grouped]
-    adjacency_tensors = [np.asarray(data, np.int64) for data in adjacency_tensors]
+        # TODO: filter out extra nodes/edges
 
-    return adjacency_tensors
+        graph_samples = self.get_graph_samples(path)
 
+        with self.graph.as_default():
 
-def compute_edges_per_type(graph, incoming=True):
-
-    # n_nodes = ... #TODO: ensure ids are consecutive
-
-    # TODO: assumes node ids range from 0 to n_nodes. Ensure this assumption actually holds
-    n_nodes, n_edge_types = 10, 5
-    edges_matrix = np.zeros((n_nodes, n_edge_types), np.int64)
-
-    if incoming:
-        for e in graph.edge: edges_matrix[e.destinationId, e.type] += 1
-    else:
-        for e in graph.edge: edges_matrix[e.sourceId, e.type] += 1
-
-    return edges_matrix
-
-
-
-
-def compute_initial_node_representation(graph):
-
-    # Extract set of all sub-tokens and create their vocabulary
-    all_tokens = []
-
-    for n in graph.node:
-        all_tokens += split_identifier_into_parts(n.contents)
-    all_tokens = list(set(all_tokens))
-
-    vocabulary = Vocabulary.create_vocabulary(all_tokens, max_size=100, count_threshold=0, add_unk=True)
-    max_size = 50
-    padding_element = 0
-
-    node_representations = np.array([vocabulary.get_id_or_unk_multiple(split_identifier_into_parts(node.contents), max_size, padding_element)
-                            for node in graph.node])
-
-    return node_representations
-
-
+            for iteration in range(10):
+                for graph in graph_samples:
+                    loss = self.sess.run([self.train_loss, self.train_step], feed_dict=graph)
+                    print("Loss:", loss)
 
 
 
 def main(path):
 
-  with open(path, "rb") as f:
-
-    g = Graph()
-    g.ParseFromString(f.read())
-
-    m = model()
-    m.sample_train()
+  m = model()
+  m.train(path)
 
 
-
-
-main("/Users/AdminDK/Dropbox/Part III Modules/R252 Machine Learning "
-     "for Programming/Practicals/features-javac-master/Test.java.proto")
+main("/Users/AdminDK/Desktop/sample_graphs")
 
 
 
