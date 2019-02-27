895ad5540e85be2c0a64a1571eb99d47333aa58b
Dmitry Kazhdan <dmitry@kazhdan.net>
2019-02-27 23:56:37 +0000

    Minor refactoring of 'main' code

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
-import tensorflow as tf
-from graph_pb2 import Graph
-from graph_pb2 import FeatureNode, FeatureEdge
-from dpu_utils.tfmodels import SparseGGNN
-import numpy as np
-from collections import defaultdict
 import os
-import vocabulary_extractor, graph_preprocessing
-import matplotlib.pyplot as plt
+import vocabulary_extractor
 from random import shuffle
 from shutil import copyfile
-import math
+import model
+
+
+# Relevant Paths:
+corpus_path = "/Users/AdminDK/Dropbox/Part III Modules/R252 Machine Learning for Programming/corpus/r252-corpus-features"
+checkpoint_path = "/Users/AdminDK/Dropbox/Part III Modules/R252 Machine Learning for Programming/Project/checkpoint/train.ckpt"
+train_path = "/Users/AdminDK/Desktop/train_graphs"
+test_path = "/Users/AdminDK/Desktop/test_graphs"
+token_path = "/Users/AdminDK/Desktop/tokens.txt"
 
 
 
 def main():
 
   # Training:
-  n_train_epochs = 60
   vocabulary = vocabulary_extractor.create_vocabulary_from_corpus(train_path, token_path)
   m = model('train', vocabulary)
+  n_train_epochs = 60
   m.train(train_path, n_train_epochs)
+
 
 
   # Inference
   vocabulary = vocabulary_extractor.load_vocabulary(token_path)
   m = model('infer', vocabulary)
-  test_acc = m.infer(test_path)
-
-
-
+  m.infer(test_path)
 
 
 
@@ -38,12 +37,15 @@ def get_train_and_test(corpus_path, train_path, test_path):
 
     f_names = []
 
+    # Extract all filenames from corpus folders
     for dirpath, dirs, files in os.walk(corpus_path):
         for filename in files:
             if filename[-5:] == 'proto':
                 fname = os.path.join(dirpath, filename)
                 f_names.append(fname)
 
+
+    # Copy subset of samples into training/testing directories
     n_samples = 100
     n_train = round(n_samples * 0.8)
     shuffle(f_names)
@@ -51,7 +53,6 @@ def get_train_and_test(corpus_path, train_path, test_path):
     train_samples = f_names[:n_train]
     test_samples = f_names[n_train:n_samples]
 
-    print(train_samples)
 
     for src in train_samples:
         dst = os.path.join(train_path, os.path.basename(src))
@@ -63,22 +64,9 @@ def get_train_and_test(corpus_path, train_path, test_path):
         copyfile(src, dst)
 
 
-corpus_path = "/Users/AdminDK/Dropbox/Part III Modules/R252 Machine Learning for Programming/Practicals/corpus/r252-corpus-features"
-checkpoint_path = "/Users/AdminDK/Dropbox/Part III Modules/R252 Machine Learning for Programming/Project/checkpoint/train.ckpt"
-train_path = "/Users/AdminDK/Desktop/train_graphs"
-test_path = "/Users/AdminDK/Desktop/test_graphs"
-token_path = "/Users/AdminDK/Desktop/tokens.txt"
 
 
 #get_train_and_test(corpus_path, train_path, test_path)
+
 main()
 
-
-# for main_epoch in epochs:
-#     main()
-#plt.plot(epochs, main_accuracies)
-# plt.scatter(epochs, main_accuracies, marker='x', color='red')
-# plt.xlabel('Training Epochs')
-# plt.ylabel('Accuracy')
-# plt.title('Training Data Accuracy')
-# plt.show()
